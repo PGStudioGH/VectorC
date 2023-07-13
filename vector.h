a738686b4e892f722d4d5a65ea7618dd06c0c04e
PGStudioGH <80467798+PGStudioGH@users.noreply.github.com>
2023-07-13 21:03:35 +0300

    Update vector.h

--- vector.h
+++ vector.h
@@ -11,6 +11,7 @@ typedef struct vector_t*                            vector;
 vector create_vector_t(uint64_t size_of_type);
 void free_vector_t(vector vec);
 void restart_vector_t(vector vec);
+void reserve_vector_t(vector vec, uint64_t capacity);
 void push_vector_t(vector vec, void* data);
 void pop_vector_t(vector vec);
 void* element_vector_t(vector vec, uint64_t index);
@@ -18,12 +19,14 @@ void insert_vector_t(vector vec, void* data, uint64_t index);
 void erase_vector_t(vector vec, uint64_t index);
 void clear_vector_t(vector vec);
 uint64_t size_vector_t(vector vec);
+uint64_t capacity_vector_t(vector vec);
 void print_vector_t(vector vec);
 
 //Macros
 #define create_vector(type)                         create_vector_t(sizeof(type))
 #define free_vector(vector)                         free_vector_t(vector)
 #define restart_vector(vector)                      restart_vector_t(vector)
+#define reserve_vector(vector, capacity)            reserve_vector_t(vector, capacity)
 #define push_front_vector(vector, type, data)       { type temp = data; insert_vector_t(vector, &temp, 0); }
 #define push_back_vector(vector, type, data)        { type temp = data; push_vector_t(vector, &temp); }
 #define pop_front_vector(vector)                    erase_vector_t(vector, 0)
@@ -33,6 +36,7 @@ void print_vector_t(vector vec);
 #define erase_vector(vector, index)                 erase_vector_t(vector, index)
 #define clear_vector(vector)                        clear_vector_t(vector)
 #define size_vector(vector)                         size_vector_t(vector)
+#define capacity_vector(vector)                     capacity_vector_t(vector)
 #define print_vector(vector)                        print_vector_t(vector)
 
 #endif
