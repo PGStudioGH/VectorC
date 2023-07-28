1de54a77e15227744585691b155b80d465a51717
PGStudioGH <80467798+PGStudioGH@users.noreply.github.com>
2023-07-28 15:39:11 +0300

    Update VectorC.h

--- VectorC.h
+++ VectorC.h
@@ -1,27 +1,21 @@
-#ifndef VECTORC89_H
-#define VECTORC89_H
+#ifndef VECTOR_C_H
+#define VECTOR_C_H
 
 /** How install VectorC.h? **
  * 1. Just download VectorC.h and put on own project C
- * 2. Include VectorC.h using code: "#include "VectorC.h""
+ * 2. Include VectorC.h using code: #include "VectorC.h"
  * 3. Done
  *
- * Attention! This header can be work only in compiler for C (C89 and latest), not C++.
+ * This header can be work only in many compiler for C (C89 and latest) and C++ (C++98 and latest).
  **/
 
 /** How use vector? **
  * 1. Use macro "template_vector(type);" before using vector
- * 2. Create vector using macro "vector(type, name_vector);". Even you can create vector without virtual methods using macro "vector_directly(type, name_vector);". See difference:
- // Vector with virtual methods //
- vector(int, v1);
- v1.methods->push(v1.self, 500);
- // Vector without virtual methods //
- vector_directly(int, v2);
- v2.push(v2.self, 500);
- * 3. You can use methods: "name_vector.methods->name_method(...);"
+ * 2. Create vector using macro "vector(type, name_vector);"
+ * 3. You can use methods: "name_vector.name_method(...);"
  * 4. You can use operator [], just use macro "DATA(name_vector, type)[index]" or create temporary variable "type* name_variable = DATA(name_vector, type); name_variable[index] = ...;"
  * 5. With enjoy :)
- * 6. But dont forget to free vector using macro "free_vector(name_vector);"
+ * 6. But dont forget to free vector using macro "free_vector(name_vector);", because C don't have auto-deconstructor
  *
  * Methods:
  * restart(self);
@@ -44,10 +38,29 @@
  * P.S. Please look file example.c
  **/
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 /* Includes */
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
+/* Declarations */
+struct _vector_data;
+void _restart_vector(void* self);
+void _clear_vector(void* self);
+void _pop_back_vector(void* self);
+unsigned int _size_vector(void* self);
+unsigned int _capacity_vector(void* self);
+#define _TEMPLATE_FUNCTION(T)\
+  void _reserve_vector_##T(void* self, unsigned int capacity);\
+  void _push_back_vector_##T(void* self, T data);\
+  void _insert_vector_##T(void* self, unsigned int index, T data);\
+  void _erase_vector_##T(void* self, unsigned int index);\
+  void _print_data_vector_##T(void* self);\
+  struct _vector_##T;
 
 /* Struct classic vector */
 struct _vector_data
@@ -57,97 +70,41 @@ struct _vector_data
   void* data;
 };
 
-/* Struct virtual table for vector */
-struct _vector_vtable
-{
-  void (*restart)();
-  void (*clear)();
-  void (*reserve)();
-  void (*push)();
-  void (*pop)();
-  void (*insert)();
-  void (*erase)();
-  unsigned int (*size)();
-  unsigned int (*capacity)();
-  void (*print_data)();
-};
-
-/* Interface vector */
-struct _vector
-{
-  struct _vector_data* self;
-  const struct _vector_vtable* methods;
-};
-struct _vector_directly
-{
-    struct _vector_data* self;
-    void (*restart)();
-    void (*clear)();
-    void (*reserve)();
-    void (*push)();
-    void (*pop)();
-    void (*insert)();
-    void (*erase)();
-    unsigned int (*size)();
-    unsigned int (*capacity)();
-    void (*print_data)();
-};
-
 /* Functions without template */
-void _init_vector_data(struct _vector_data** vec)
+void _restart_vector(void* self)
 {
-  *vec = (struct _vector_data*)malloc(sizeof(struct _vector_data));
-  if (*vec != NULL)
-  {
-    (*vec)->size = 0;
-    (*vec)->capacity = 0;
-    (*vec)->data = NULL;
-  } 
+  struct _vector_data* vec = (struct _vector_data*)self;
+  vec->size = 0;
+  vec->capacity = 0;
+  free(vec->data);
+  vec->data = NULL;
 }
-void _free_vector_data(struct _vector_data** vec)
+void _clear_vector(void* self)
 {
-  if (*vec != NULL)
-  {
-    free((*vec)->data);
-    free(*vec);
-    *vec = NULL;
-  }
+  struct _vector_data* vec = (struct _vector_data*)self;
+  vec->size = 0;
 }
-void _restart_vector(struct _vector_data* vec)
+void _pop_back_vector(void* self)
 {
-  if (vec != NULL)
-  {
-    vec->size = 0;
-    vec->capacity = 0;
-    free(vec->data);
-    vec->data = NULL;
-  }
-}
-void _clear_vector(struct _vector_data* vec)
-{
-  if (vec != NULL)
-  {
-    vec->size = 0;
-  }
-}
-void _pop_back_vector(struct _vector_data* vec)
-{
-  if (vec != NULL && vec->size > 0)
+  struct _vector_data* vec = (struct _vector_data*)self;
+  if (vec->size > 0)
   {
     vec->size--;
   }
 }
-unsigned int _size_vector(struct _vector_data* vec)
+unsigned int _size_vector(void* self)
 {
-  if (vec != NULL && vec->data != NULL)
+  struct _vector_data* vec = (struct _vector_data*)self;
+  if (vec->data != NULL)
   {
     return vec->size;
   }
   return 0;
 }
-unsigned int _capacity_vector(struct _vector_data* vec)
+unsigned int _capacity_vector(void* self)
 {
-  if (vec != NULL && vec->data != NULL)
+  struct _vector_data* vec = (struct _vector_data*)self;
+  if (vec->data != NULL)
   {
     return vec->capacity;
   }
@@ -156,65 +113,66 @@ unsigned int _capacity_vector(struct _vector_data* vec)
 
 /* Functions with template */
 #define vector(T, V)\
-  struct _vector V;\
-  _init_vector_data(&V.self);\
-  V.methods = &_vector_vtable_##T;
-
-#define vector_directly(T, V)\
-    struct _vector_directly V;\
-    _init_vector_data(&V.self);\
-    *(struct _vector_vtable*)((void*)&V.self + sizeof(V.self)) = _vector_vtable_##T;
+  const struct _vector_##T V =\
+  {\
+    calloc(1, sizeof(struct _vector_data)),\
+    _restart_vector, _clear_vector,\
+    _reserve_vector_##T, _push_back_vector_##T,\
+    _pop_back_vector,\
+    _insert_vector_##T, _erase_vector_##T,\
+    _size_vector, _capacity_vector,\
+    _print_data_vector_##T\
+  };
 
 #define free_vector(V)\
-  _free_vector_data(&V.self);\
-  memset(&V, 0, sizeof(V));
+  free(((struct _vector_data*)V.self)->data);\
+  free(V.self);
 
-#define DATA(V, T) ((T*)V.self->data)
+#define DATA(V, T)\
+  ((T*)((struct _vector_data*)V.self)->data)
 
 #define template_vector(T)\
-  void _reserve_vector_##T(struct _vector_data* vec, unsigned int capacity)\
+  _TEMPLATE_FUNCTION(T)\
+  void _reserve_vector_##T(void* self, unsigned int capacity)\
   {\
-    if (vec != NULL)\
+    struct _vector_data* vec = (struct _vector_data*)self;\
+    void* temp = realloc(vec->data, capacity * sizeof(T));\
+    if (temp != NULL)\
     {\
-      void* temp = realloc(vec->data, capacity * sizeof(T));\
+      vec->capacity = capacity;\
+      vec->data = temp;\
+    }\
+  }\
+  void _push_back_vector_##T(void* self, T data)\
+  {\
+    struct _vector_data* vec = (struct _vector_data*)self;\
+    if (vec->data == NULL)\
+    {\
+      vec->data = malloc(sizeof(T));\
+      if (vec->data != NULL)\
+      {\
+        vec->capacity = 1;\
+      }\
+      else return;\
+    }\
+    \
+    if (vec->size == vec->capacity)\
+    {\
+      void* temp = realloc(vec->data, (vec->capacity << 1) * sizeof(T));\
       if (temp != NULL)\
       {\
-        vec->capacity = capacity;\
+        vec->capacity <<= 1;\
         vec->data = temp;\
       }\
+      else return;\
     }\
+    *((T*)vec->data + vec->size) = data;\
+    vec->size++;\
   }\
-  void _push_back_vector_##T(struct _vector_data* vec, T data)\
+  void _insert_vector_##T(void* self, unsigned int index, T data)\
   {\
-    if (vec != NULL)\
-    {\
-      if (vec->data == NULL)\
-      {\
-        vec->data = malloc(sizeof(T));\
-        if (vec->data != NULL)\
-        {\
-          vec->capacity = 1;\
-        }\
-        else return;\
-      }\
-      \
-      if (vec->size == vec->capacity)\
-      {\
-        void* temp = realloc(vec->data, (vec->capacity << 1) * sizeof(T));\
-        if (temp != NULL)\
-        {\
-          vec->capacity <<= 1;\
-          vec->data = temp;\
-        }\
-        else return;\
-      }\
-      *((T*)vec->data + vec->size) = data;\
-      vec->size++;\
-    }\
-  }\
-  void _insert_vector_##T(struct _vector_data* vec, unsigned int index, T data)\
-  {\
-    if (vec != NULL && index <= vec->size)\
+    struct _vector_data* vec = (struct _vector_data*)self;\
+    if (index <= vec->size)\
     {\
       if (vec->data == NULL)\
       {\
@@ -242,18 +200,20 @@ unsigned int _capacity_vector(struct _vector_data* vec)
       vec->size++;\
     }\
   }\
-  void _erase_vector_##T(struct _vector_data* vec, unsigned int index)\
+  void _erase_vector_##T(void* self, unsigned int index)\
   {\
-    if (vec != NULL && vec->data != NULL && index < vec->size)\
+    struct _vector_data* vec = (struct _vector_data*)self;\
+    if (vec->data != NULL && index < vec->size)\
     {\
       T* current_data = (T*)vec->data + index;\
       memcpy((void*)current_data, (void*)(current_data + 1), (vec->size - index) * sizeof(T));\
       vec->size--;\
     }\
   }\
-  void _print_data_vector_##T(struct _vector_data* vec)\
+  void _print_data_vector_##T(void* self)\
   {\
-    if (vec != NULL && vec->data != NULL && vec->size != 0)\
+    struct _vector_data* vec = (struct _vector_data*)self;\
+    if (vec->data != NULL && vec->size != 0)\
     {\
       printf("Data:\n");\
       unsigned int i = 0;\
@@ -272,14 +232,23 @@ unsigned int _capacity_vector(struct _vector_data* vec)
     }\
     else printf("Empty vector\n");\
   }\
-  const static struct _vector_vtable _vector_vtable_##T = \
+  struct _vector_##T\
   {\
-    (void (*)())_restart_vector, (void (*)())_clear_vector,\
-    (void (*)())_reserve_vector_##T, (void (*)())_push_back_vector_##T,\
-    (void (*)())_pop_back_vector,\
-    (void (*)())_insert_vector_##T, (void (*)())_erase_vector_##T,\
-    (unsigned int (*)())_size_vector, (unsigned int (*)())_capacity_vector,\
-    (void (*)())_print_data_vector_##T\
-  };
+    void* const self;\
+    void (*const restart)(void*);\
+    void (*const clear)(void*);\
+    void (*const reserve)(void*, unsigned int);\
+    void (*const push)(void*, T);\
+    void (*const pop)(void*);\
+    void (*const insert)(void*, unsigned int, T);\
+    void (*const erase)(void*, unsigned int);\
+    unsigned int (*const size)(void*);\
+    unsigned int (*const capacity)(void*);\
+    void (*const print_data)(void*);\
+  }
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
